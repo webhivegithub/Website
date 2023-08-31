fcddcc6325685b17963894536d9fe275a2db8f42
JackHughes03 <jwhughes03@yahoo.com>
2023-08-31 21:41:41 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Website/",
   plugins: [vue()],
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-vite-tailwind/' : '/',
+  resolve: {
+    alias: [
+      
+    ]
+  }
 })
